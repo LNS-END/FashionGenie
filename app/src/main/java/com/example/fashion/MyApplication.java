c3d12bbd20624996047a4364f8087e4bffbe4a49
lmomj1288 <114644467+lmomj1288@users.noreply.github.com>
2023-06-23 13:30:15 +0900

    Update MyApplication.java

--- app/src/main/java/com/example/fashion/MyApplication.java
+++ app/src/main/java/com/example/fashion/MyApplication.java
@@ -1,250 +1,80 @@
-package com.example.fashion;
+package com.example.fashingenie;
 
 import android.app.Application;
-import android.util.Log;
-
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
-
 import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
+import android.util.Log;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
-
-
+import retrofit2.Callback;
+import retrofit2.Response;
+import java.io.IOException;
+import java.util.List;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ExecutorService;
+import androidx.annotation.NonNull;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 
 
 public class MyApplication extends Application {
+
+    private ExecutorService executorService;
+
     @Override
     public void onCreate() {
         super.onCreate();
 
-        // Initialize and load your data here
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-                loadInitialData();
-            }
-        }).start();
+        executorService = Executors.newSingleThreadExecutor();
+        loadInitialData();
     }
 
+
     private void loadInitialData() {
-        Retrofit retrofit = new Retrofit.Builder()
-                .baseUrl( "https://wasincome.run.goorm.site/v1data/")
-                .addConverterFactory(GsonConverterFactory.create())
-                .build();
+        executorService.execute(() -> {
+            // Prepare your Retrofit instance
+            Gson gson = new GsonBuilder()
+                    .excludeFieldsWithoutExposeAnnotation()
+                    .create();
 
-        MyAPI api = retrofit.create(MyAPI.class);
+            // Prepare your Retrofit instance
+            Retrofit retrofit = new Retrofit.Builder()
+                    .baseUrl("https://wasincome.run.goorm.site/v1data/")
+                    .addConverterFactory(GsonConverterFactory.create(gson))
+                    .build();
 
-        // For each class of item (Bag, HeadWear, Outer, Pants, Shoes, Top, Coordi, LikedCoordi)
-        List<Bag> bagList = new ArrayList<>();
-        Bag bag = new Bag();
 
-        bag.setID(1);
-        bag.setImagePath("NULL");
-        bag.setCategory("Sports");
-        bag.setBrand("Nike");
-        bag.setSeason("Summer");
-        bag.setStyle("Sports");
-        bag.setTextile("soft");
-        bag.setColor("red");
-        bagList.add(bag);
+            MyAPI api = retrofit.create(MyAPI.class);
 
-        api.sendBagData(bagList).enqueue(new Callback<Void>() {
-            @Override
-            public void onResponse(Call<Void> call, Response<Void> response) {
-                // Handle the response...
-                if (response.isSuccessful()) {
-                    Log.e("MyAPI", "Request succeeded");
-                } else {
-                    Log.e("MyAPI", "Server returned an error: " + response.code());
-                }
-            }
+            // Get a reference to your database
+            DataBase database = DataBase.getInstance(getApplicationContext());
 
-            @Override
-            public void onFailure(Call<Void> call, Throwable t) {
-                // Handle the failure...
-                if (t instanceof IOException) {
-                    // A network or conversion error happened
-                    Log.e("MyAPI", "Network error", t);
-                } else {
-                    // Some other kind of error happened
-                    Log.e("MyAPI", "Some other error happened", t);
-                }
-            }
+            List<Bag> bag = database.bagDao().getBagAll();
+            List<HeadWear> headWear = database.headWearDao().getHeadWearAll();
+            List<Outer> outers = database.outerDao().getOuterAll();
+            List<Pants> pants = database.pantsDao().getPantsAll();
+            List<Shoes> shoes = database.shoesDao().getShoesAll();
+            List<Top> tops = database.topDao().getTopAll();
+
+            sendAllDataToServer(api, bag, headWear, outers, pants, shoes, tops);
         });
-        List<HeadWear> headWearList = new ArrayList<>();
-        HeadWear headwear = new HeadWear();
-        // Initialize the headwear...
-        headwear.setID(1);
-        headwear.setImagePath("NULL");
-        headwear.setCategory("Sports");
-        headwear.setBrand("Nike");
-        headwear.setSeason("Summer");
-        headwear.setStyle("Sports");
-        headwear.setTextile("soft");
-        headwear.setColor("red");
-        headWearList.add(headwear);
+    }
 
-        api.sendHeadwearData(headWearList).enqueue(new Callback<Void>() {
+    private void sendAllDataToServer(MyAPI api, List<Bag> bags, List<HeadWear> headWears, List<Outer> outers
+    ,List<Pants> pants,List<Shoes> shoes,List<Top> tops) {
+        // For bags
+        api.sendBagData(bags).enqueue(new Callback<Void>() {
             @Override
-            public void onResponse(Call<Void> call, Response<Void> response) {
+            public void onResponse(@NonNull Call<Void> call,@NonNull Response<Void> response) {
                 // Handle the response...
                 if (response.isSuccessful()) {
-                    Log.e("MyAPI", "Request succeeded");
+                    Log.e("MyAPI", "Bags sent successfully");
                 } else {
                     Log.e("MyAPI", "Server returned an error: " + response.code());
                 }
             }
 
             @Override
-            public void onFailure(Call<Void> call, Throwable t) {
-                // Handle the failure...
-                if (t instanceof IOException) {
-                    // A network or conversion error happened
-                    Log.e("MyAPI", "Network error", t);
-                } else {
-                    // Some other kind of error happened
-                    Log.e("MyAPI", "Some other error happened", t);
-                }
-            }
-        });
-        List<com.example.fashion.Outer> outerList = new ArrayList<>();
-        com.example.fashion.Outer outer  = new com.example.fashion.Outer();
-        // Initialize the outer...
-        outer.setID(1);
-        outer.setImagePath("NULL");
-        outer.setCategory("Sports");
-        outer.setBrand("Nike");
-        outer.setSeason("Summer");
-        outer.setStyle("Sports");
-        outer.setTextile("soft");
-        outer.setColor("red");
-        outerList.add(outer);
-
-        api.sendOuterData(outerList).enqueue(new Callback<Void>() {
-            @Override
-            public void onResponse(Call<Void> call, Response<Void> response) {
-                // Handle the response...
-                if (response.isSuccessful()) {
-                    Log.e("MyAPI", "Request succeeded");
-                } else {
-                    Log.e("MyAPI", "Server returned an error: " + response.code());
-                }
-            }
-
-            @Override
-            public void onFailure(Call<Void> call, Throwable t) {
-                // Handle the failure...
-                if (t instanceof IOException) {
-                    // A network or conversion error happened
-                    Log.e("MyAPI", "Network error", t);
-                } else {
-                    // Some other kind of error happened
-                    Log.e("MyAPI", "Some other error happened", t);
-                }
-            }
-        });
-        List<Pants> pantsList = new ArrayList<>();
-        Pants pants = new Pants();
-        // Initialize the pants...
-        pants.setID(1);
-        pants.setImagePath("NULL");
-        pants.setCategory("Sports");
-        pants.setBrand("Nike");
-        pants.setSeason("Summer");
-        pants.setStyle("Sports");
-        pants.setTextile("soft");
-        pants.setColor("red");
-        pantsList.add(pants);
-
-        api.sendPantsData(pantsList).enqueue(new Callback<Void>() {
-            @Override
-            public void onResponse(Call<Void> call, Response<Void> response) {
-                // Handle the response...
-                if (response.isSuccessful()) {
-                    Log.e("MyAPI", "Request succeeded");
-                } else {
-                    Log.e("MyAPI", "Server returned an error: " + response.code());
-                }
-            }
-
-            @Override
-            public void onFailure(Call<Void> call, Throwable t) {
-                // Handle the failure...
-                if (t instanceof IOException) {
-                    // A network or conversion error happened
-                    Log.e("MyAPI", "Network error", t);
-                } else {
-                    // Some other kind of error happened
-                    Log.e("MyAPI", "Some other error happened", t);
-                }
-            }
-        });
-        List<com.example.fashion.Shoes> shoesList = new ArrayList<>();
-        com.example.fashion.Shoes shoes  = new com.example.fashion.Shoes();
-        // Initialize the shoes...
-        shoes.setID(1);
-        shoes.setImagePath("NULL");
-        shoes.setCategory("Sports");
-        shoes.setBrand("Nike");
-        shoes.setSeason("Summer");
-        shoes.setStyle("Sports");
-        shoes.setTextile("soft");
-        shoes.setColor("red");
-        shoesList.add(shoes);
-
-        api.sendShoesData(shoesList).enqueue(new Callback<Void>() {
-            @Override
-            public void onResponse(Call<Void> call, Response<Void> response) {
-                // Handle the response...
-                if (response.isSuccessful()) {
-                    Log.e("MyAPI", "Request succeeded");
-                } else {
-                    Log.e("MyAPI", "Server returned an error: " + response.code());
-                }
-            }
-
-            @Override
-            public void onFailure(Call<Void> call, Throwable t) {
-                // Handle the failure...
-                if (t instanceof IOException) {
-                    // A network or conversion error happened
-                    Log.e("MyAPI", "Network error", t);
-                } else {
-                    // Some other kind of error happened
-                    Log.e("MyAPI", "Some other error happened", t);
-                }
-            }
-        });
-        List<com.example.fashion.Top> topList = new ArrayList<>();
-        com.example.fashion.Top top  = new com.example.fashion.Top();
-        // Initialize the top...
-        top.setID(1);
-        top.setImagePath("NULL");
-        top.setCategory("Sports");
-        top.setBrand("Nike");
-        top.setSeason("Summer");
-        top.setStyle("Sports");
-        top.setTextile("soft");
-        top.setColor("red");
-        topList.add(top);
-
-        api.sendTopData(topList).enqueue(new Callback<Void>() {
-            @Override
-            public void onResponse(Call<Void> call, Response<Void> response) {
-                // Handle the response...
-                if (response.isSuccessful()) {
-                    Log.e("MyAPI", "Request succeeded");
-                } else {
-                    Log.e("MyAPI", "Server returned an error: " + response.code());
-                }
-            }
-
-            @Override
-            public void onFailure(Call<Void> call, Throwable t) {
+            public void onFailure(@NonNull Call<Void> call, @NonNull Throwable t) {
                 // Handle the failure...
                 if (t instanceof IOException) {
                     // A network or conversion error happened
@@ -256,56 +86,124 @@ public class MyApplication extends Application {
             }
         });
 
-//        Coordi coordi   = new Coordi();
-        // Initialize the top...
-//        api.sendCoordiData(coordi).enqueue(new Callback<Void>() {
-//            @Override
-//            public void onResponse(Call<Void> call, Response<Void> response) {
+        // Repeat for all your item types... in this case, HeadWear
+        api.sendHeadwearData(headWears).enqueue(new Callback<Void>() {
+            @Override
+            public void onResponse(@NonNull Call<Void> call, @NonNull Response<Void> response) {
                 // Handle the response...
-//                if (response.isSuccessful()) {
-//                    Log.e("MyAPI", "Request succeeded");
-//                } else {
- //                   Log.e("MyAPI", "Server returned an error: " + response.code());
- //               }
- //           }
+                if (response.isSuccessful()) {
+                    Log.e("MyAPI", "HeadWears sent successfully");
+                } else {
+                    Log.e("MyAPI", "Server returned an error: " + response.code());
+                }
+            }
 
-//            @Override
-//            public void onFailure(Call<Void> call, Throwable t) {
+            @Override
+            public void onFailure(@NonNull Call<Void> call, @NonNull Throwable t) {
                 // Handle the failure...
-//                if (t instanceof IOException) {
+                if (t instanceof IOException) {
                     // A network or conversion error happened
-//                    Log.e("MyAPI", "Network error", t);
- //               } else {
+                    Log.e("MyAPI", "Network error", t);
+                } else {
                     // Some other kind of error happened
- //                   Log.e("MyAPI", "Some other error happened", t);
- //               }
- //           }
- //       });
+                    Log.e("MyAPI", "Some other error happened", t);
+                }
+            }
+        });
 
-//        LikedCoordi likedCoordi   = new LikedCoordi();
-        // Initialize the top...
- //       api.sendLikedCoordiData(likedCoordi).enqueue(new Callback<Void>() {
- //           @Override
- //           public void onResponse(Call<Void> call, Response<Void> response) {
+        api.sendOuterData(outers).enqueue(new Callback<Void>() {
+            @Override
+            public void onResponse(@NonNull Call<Void> call, @NonNull Response<Void> response) {
                 // Handle the response...
- //               if (response.isSuccessful()) {
- //                   Log.e("MyAPI", "Request succeeded");
-  //              } else {
-  //                  Log.e("MyAPI", "Server returned an error: " + response.code());
- //               }
- //           }
+                if (response.isSuccessful()) {
+                    Log.e("MyAPI", "Bags sent successfully");
+                } else {
+                    Log.e("MyAPI", "Server returned an error: " + response.code());
+                }
+            }
 
- //           @Override
-  //          public void onFailure(Call<Void> call, Throwable t) {
+            @Override
+            public void onFailure(@NonNull Call<Void> call,@NonNull  Throwable t) {
                 // Handle the failure...
-  //              if (t instanceof IOException) {
+                if (t instanceof IOException) {
                     // A network or conversion error happened
-  //                  Log.e("MyAPI", "Network error", t);
-  //              } else {
+                    Log.e("MyAPI", "Network error", t);
+                } else {
                     // Some other kind of error happened
- //                   Log.e("MyAPI", "Some other error happened", t);
-  //              }
- //           }
-//        });
+                    Log.e("MyAPI", "Some other error happened", t);
+                }
+            }
+        });
+
+        api.sendPantsData(pants).enqueue(new Callback<Void>() {
+            @Override
+            public void onResponse(@NonNull Call<Void> call, @NonNull Response<Void> response) {
+                // Handle the response...
+                if (response.isSuccessful()) {
+                    Log.e("MyAPI", "Bags sent successfully");
+                } else {
+                    Log.e("MyAPI", "Server returned an error: " + response.code());
+                }
+            }
+
+            @Override
+            public void onFailure(@NonNull Call<Void> call,@NonNull  Throwable t) {
+                // Handle the failure...
+                if (t instanceof IOException) {
+                    // A network or conversion error happened
+                    Log.e("MyAPI", "Network error", t);
+                } else {
+                    // Some other kind of error happened
+                    Log.e("MyAPI", "Some other error happened", t);
+                }
+            }
+        });
+
+        api.sendShoesData(shoes).enqueue(new Callback<Void>() {
+            @Override
+            public void onResponse(@NonNull Call<Void> call, @NonNull Response<Void> response) {
+                // Handle the response...
+                if (response.isSuccessful()) {
+                    Log.e("MyAPI", "Bags sent successfully");
+                } else {
+                    Log.e("MyAPI", "Server returned an error: " + response.code());
+                }
+            }
+
+            @Override
+            public void onFailure(@NonNull Call<Void> call, @NonNull Throwable t) {
+                // Handle the failure...
+                if (t instanceof IOException) {
+                    // A network or conversion error happened
+                    Log.e("MyAPI", "Network error", t);
+                } else {
+                    // Some other kind of error happened
+                    Log.e("MyAPI", "Some other error happened", t);
+                }
+            }
+        });
+        api.sendTopData(tops).enqueue(new Callback<Void>() {
+            @Override
+            public void onResponse(@NonNull Call<Void> call, @NonNull Response<Void> response) {
+                // Handle the response...
+                if (response.isSuccessful()) {
+                    Log.e("MyAPI", "Bags sent successfully");
+                } else {
+                    Log.e("MyAPI", "Server returned an error: " + response.code());
+                }
+            }
+
+            @Override
+            public void onFailure(@NonNull Call<Void> call, @NonNull Throwable t) {
+                // Handle the failure...
+                if (t instanceof IOException) {
+                    // A network or conversion error happened
+                    Log.e("MyAPI", "Network error", t);
+                } else {
+                    // Some other kind of error happened
+                    Log.e("MyAPI", "Some other error happened", t);
+                }
+            }
+        });
     }
 }
